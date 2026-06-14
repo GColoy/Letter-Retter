dbd55569ae6c76332133579f42ef895a5939eef3
Oliver Jeschke <oliver.jeschke@stud.tu-darmstadt.de>
2026-06-14 16:04:08 +0200

    More Scenes added + Transitions

--- Assets/Scripts/InteractBoxAndDialogue.cs
+++ Assets/Scripts/InteractBoxAndDialogue.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
+using System;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic; 
 
 public class InteractBoxAndDialogue : MonoBehaviour
 {
     public GameObject eBox;
     public Dialogue dialogue;
     public InputAction interact;
-    private string[] textToShow = {"Test", "Bla"};
+    public GameObject door;
+    public VisualizeKeyFinger keyboard;
+    //private string[] textToShow = {"Test", "Bla"};
     private bool isInRange;
     SceneChanger sceneChanger = new SceneChanger();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        GameObject enemy = GameObject.FindWithTag("Enemy");
+        if(enemy != null){
+            Debug.Log("Jup");
+            enemy.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else
+        {
+            Debug.Log("Nope");
+        }
         eBox.SetActive(false);
+        if(door != null)
+        {
+            door.SetActive(false);
+        }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -49,8 +67,83 @@ public class InteractBoxAndDialogue : MonoBehaviour
         {
             if (interact.ReadValue<float>() > 0f)
             {
-                dialogue.Show(textToShow, sceneChanger);
+                string nameOfChar =  gameObject.name;
+                string[] textToShow = null;
+                switch (nameOfChar)
+                {
+                    case "EnterKey":
+                        textToShow = LoadDialog("enterkey");
+                        dialogue.Show(textToShow, new ActionAfterDialog(() =>
+                            {
+                            if (door != null) door.SetActive(true);
+                            }));
+                        break;
+                    case "Door":
+                        SceneManager.LoadScene("FJFinding");
+                        break;
+                    case "FJChar":
+                        textToShow = LoadDialog("FJplusEnemy1");
+                        dialogue.Show(textToShow, new ActionAfterDialog(() =>
+                            {
+                                List<string> keys = new List<string> { "F", "J" };
+                                keyboard.ShowKeys(keys);
+                                string[] textToShow2 = LoadDialog("FJplusEnemy2");
+                                dialogue.Show(textToShow2, new ActionAfterDialog(() =>
+                                    {
+                                        keyboard.HideOverlay();
+                                        GameObject enemy1 = GameObject.FindWithTag("Enemy");
+                                        if(enemy1 != null){
+                                            Debug.Log("Jup");
+                                            enemy1.GetComponent<SpriteRenderer>().enabled = true;
+                                        }
+                                        else
+                                        {
+                                            Debug.Log("Nope");
+                                        }
+                                        string[] textToShow3 = LoadDialog("FJplusEnemy3");
+                                        dialogue.Show(textToShow3, new ActionAfterDialog(() =>
+                                            {
+                                                SceneManager.LoadScene("CombatScreen");
+                                            }));
+                                    }
+                                ));
+                            }));
+                        break;
+                    default:
+                        textToShow = new string[0];
+                        break;
+                }
             }
         }
     }
+    public static string[] LoadDialog(string key) {
+        TextAsset ta = Resources.Load<TextAsset>("text"); // omit .json
+        if (ta == null) return null;
+        DialogCollection data = JsonUtility.FromJson<DialogCollection>(ta.text);
+        switch (key) {
+            case "enterkey": return data.enterkey;
+            case "enemyone": return data.enemyone;
+            case "FJplusEnemy1": return data.FJplusEnemy1;
+            case "FJplusEnemy2": return data.FJplusEnemy2;
+            case "FJplusEnemy3": return data.FJplusEnemy3;
+            // add more cases as needed
+            default: return null;
+        }
+    }
 }
+
+class ActionAfterDialog : IAfterDialogAction
+{
+    private readonly Action _a;
+    public ActionAfterDialog(Action a) => _a = a;
+    public void action() => _a();
+}
+public class DialogCollection {
+    public string[] enterkey;
+    public string[] FJplusEnemy1;
+    public string[] FJplusEnemy2;
+    public string[] FJplusEnemy3;
+    public string[] enemyone;
+    // add more fields for other keys
+}
+
