d55472e5287e4906c7f0f012b66dae42d78e5fd7
felix <felix.camrath@gmail.com>
2026-06-15 20:04:57 +0200

    more advanced random letter provider

--- Assets/Scripts/CombatScreen/RandomLetterChallengeProvider.cs
+++ Assets/Scripts/CombatScreen/RandomLetterChallengeProvider.cs
@@ -9,11 +9,20 @@ namespace CombatScreen
     class RandomLetterChallengeProvider : ChallengeProvider
     {
         [SerializeField] private string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        [SerializeField] private int minStringLength = 3;
+        [SerializeField] private int maxStringLength = 8;
+
 
         public override string getNextChallenge()
         {
-            int i = Random.Range(0, alphabet.Length);
-            return alphabet[i].ToString();
+            int length = Random.Range(minStringLength, maxStringLength + 1);
+            var builder = new System.Text.StringBuilder(length);
+            for (int n = 0; n < length; n++)
+            {
+                int i = Random.Range(0, alphabet.Length);
+                builder.Append(alphabet[i]);
+            }
+            return builder.ToString();
         }
     }
 }
